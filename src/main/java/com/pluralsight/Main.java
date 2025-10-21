62a29793e0ea42e8bcad86816eb0c94d72430618
O-Mercy <cheria.mercy@gmail.com>
2025-10-21 13:20:26 -0500

    Reservation class complete

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,15 +2,18 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
-        Room r = new Room(1, 89.00, false, false);
-        System.out.println("Beds: " + r.getNumberOfBeds());
-        System.out.println("Price: " + r.getPrice());
-        System.out.println("Occupied: " + r.isOccupied());
-        System.out.println("Dirty: " + r.isDirty());
-        System.out.println(("Available: " + r.isAvailable()));
+        Room room1 = new Room(1, 89.00, false, false);
+        System.out.println("Beds: " + room1.getNumberOfBeds());
+        System.out.println("Price: " + room1.getPrice());
+        System.out.println("Occupied: " + room1.isOccupied());
+        System.out.println("Dirty: " + room1.isDirty());
+        System.out.println(("Available: " + room1.isAvailable()));
 
         Reservation r = new Reservation("king", 3, true);
-        System.out.println();
+        System.out.println("Room type: " + r.getRoomType());
+        System.out.println("Price per night: " + r.getPrice());
+        System.out.println("Number of nights: " + r.getNumberOfNights());
+        System.out.println("Total: " + r.getReservationTotal());
 
     }
 }
