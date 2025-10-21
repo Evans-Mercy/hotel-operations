de569363a2aad32f035a23b045773eac4ae4b5ab
O-Mercy <cheria.mercy@gmail.com>
2025-10-21 11:41:47 -0500

    Room class

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+        Room r = new Room(1, 89.00, false, false);
+        System.out.println("Beds: " + r.getNumberOfBeds());
+        System.out.println("Price: " + r.getPrice());
+        System.out.println("Occupied: " + r.isOccupied());
+        System.out.println("Dirty: " + r.isDirty());
+        System.out.println(("Available: " + r.isAvailable()));
+
+    }
+}
