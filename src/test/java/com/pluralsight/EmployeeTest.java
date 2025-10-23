f479f8569c6baf734fcde3de984da795a8e753e2
Evans-Mercy <cheria.mercy@gmail.com>
2025-10-23 13:53:48 -0500

    employee testing - happy path

--- src/test/java/com/pluralsight/EmployeeTest.java
+++ src/test/java/com/pluralsight/EmployeeTest.java
@@ -7,10 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class EmployeeTest {
 
     @Test
-    void punchIn() {
-    }
+    void punchInAndOut() {
+        //Arrange
+        Employee employee1 = new Employee(101, "Mercy", "Front Desk", 80.0, 8);
 
-    @Test
-    void punchOut() {
+        //Act
+        employee1.punchIn(9.0);
+        employee1.punchOut(18.0);
+
+        //Assert
+        assertTrue(employee1.getHoursWorked() >= 0 );
+
+
     }
 }
