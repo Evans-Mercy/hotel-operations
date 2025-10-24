6815b3ee845a23e18575afd1bff09691a4a89ab3
Evans-Mercy <cheria.mercy@gmail.com>
2025-10-24 12:57:26 -0500

    TODO's

--- BlackJack/src/main/java/com/pluralsight/Main.java
+++ BlackJack/src/main/java/com/pluralsight/Main.java
@@ -1,5 +1,11 @@
 package com.pluralsight;
 
 public class Main {
+    //TODO - Ask how many players are playing
+    // asks for each players name
+    // creates and shuffles a deck
+    // deals 2 cards to each player
+    // displays all player hands and scores
+    // determine who's closest to 21 without going over
 
 }
